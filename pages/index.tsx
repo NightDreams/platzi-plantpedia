8d2cd0872a3a2db9765ec5a0f26b56f4e2f2c6ad
jair neri <yayodcv@gmail.com>
2021-10-21 23:23:24 -0500

    C9 - SSR , setup home {hero, authors, plants}

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,35 @@
-import { useEffect, useState } from 'react'
+import { GetStaticProps, InferGetStaticPropsType } from "next";
 import { Grid } from '@ui/Grid'
 import { Button } from '@ui/Button'
 import { Typography } from '@ui/Typography'
 import { Layout } from '@components/Layout'
-import { PlantCollection } from "@components/PlantCollection";
+
 import { getPlantList } from "@api";
+import { Hero } from '@components/Hero';
+import { PlantCollection } from "@components/PlantCollection";
+import { Authors } from "@components/Authors";
 
 
+type HomeProps = { plants: Plant[] }
 
-export default function Home() {
-  const [data, setData] = useState<Plant[]>([])
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const plants = await getPlantList({ limit: 10 })
+  return {
+    props: {
+      plants,
+    }
+  }
+}
 
-  useEffect(() => {
-    getPlantList({ limit: 10 }).then(setData)
-  }, [])
-
+export default function Home({ plants }:
+  InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <Layout>
-      <PlantCollection plants={data} variant="square"></PlantCollection>
+      <Hero {...plants[4]} className="mb-20" />
+      <Authors className="mb-10" />
+      <PlantCollection plants={plants.slice(1, 3)} variant="vertical"
+        className="mb-24" />
+      <PlantCollection plants={plants.length > 8 ? plants.slice(3, 9) : plants} variant="square" />
     </Layout>
   )
 }
