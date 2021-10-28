edb2472c14e593c1a37f07c23dda6c424971e677
jair neri <yayodcv@gmail.com>
2021-10-28 16:08:12 -0500

    C11 - SSG - Authors , Categories

--- pages/entry/[slug].tsx
+++ pages/entry/[slug].tsx
@@ -1,9 +1,11 @@
 import { GetStaticProps, InferGetStaticPropsType } from "next";
+import Link from 'next/link'
 
-import { getPlant, getPlantList } from '@api'
+import { getPlant, getPlantList, getCategoryList } from '@api'
 import { Layout } from "@components/Layout";
 import { RichText } from "@components/RichText";
 import { AuthorCard } from "@components/AuthorCard";
+import { PlantEntryInline } from "@components/PlantCollection";
 
 import { Typography } from "@ui/Typography";
 import { Grid } from "@ui/Grid";
@@ -27,12 +29,13 @@ export const getStaticPaths = async () => {
   }
 }
 
-type PlantEntryProps = {
-  plant: Plant
+type PlantEntryPageProps = {
+  plant: Plant | null
+  otherEntries: Plant[] | null
+  categories: Category[] | null
 }
-
 /* Alimentacion de la pagina -  Props de la pagina */
-export const getStaticProps: GetStaticProps<PlantEntryProps> = async ({ params }) => {
+export const getStaticProps: GetStaticProps<PlantEntryPageProps> = async ({ params }) => {
   const slug = params?.slug
   if (typeof slug !== 'string') {
     return {
@@ -41,9 +44,13 @@ export const getStaticProps: GetStaticProps<PlantEntryProps> = async ({ params }
   }
   try {
     const plant = await getPlant(slug)
+    const otherEntries = await getPlantList({ limit: 5, })
+    const categories = await getCategoryList({ limit: 10 })
     return {
       props: {
-        plant
+        plant,
+        otherEntries,
+        categories
       }
     }
 
@@ -54,7 +61,15 @@ export const getStaticProps: GetStaticProps<PlantEntryProps> = async ({ params }
   }
 }
 
-export default function PlantEntryPage({ plant }: InferGetStaticPropsType<typeof getStaticProps>) {
+export default function PlantEntryPage({ plant, otherEntries, categories }: InferGetStaticPropsType<typeof getStaticProps>) {
+  if (plant == null) {
+    return (
+      <Layout>
+        <main className="pt-16 text-center"> 404 my friend </main>
+      </Layout>
+    )
+  }
+
   return (
     <Layout>
       <Grid container spacing={4}>
@@ -70,13 +85,35 @@ export default function PlantEntryPage({ plant }: InferGetStaticPropsType<typeof
           </div>
         </Grid>
         <Grid item xs={12} md={4} lg={3} component="aside">
-          <Typography variant="h5" component="h3" className="mb-4">
-            Recent Posts
-          </Typography>
+          <section>
+            <Typography variant="h5" component="h3" className="mb-4">
+              Recent Posts
+            </Typography>
+            {
+              otherEntries?.map((plantEntry) => (
+                <article className="mb-4" key={plantEntry.id}>
+                  <PlantEntryInline {...plantEntry} />
+                </article>
+              ))
+            }
+          </section>
+
           <section className="mt-10">
             <Typography variant="h5" component="h3" className="mb-4">
               Categories
             </Typography>
+            <ul className="List">
+              {categories?.map((category) => (
+                <li key={category.id}>
+                  <Link passHref href={`/category/${category.slug}`}>
+                    <Typography component="a" variant="h6">
+                      {category.title}
+                    </Typography>
+                  </Link>
+                </li>
+              ))
+              }
+            </ul>
           </section>
         </Grid>
         <section className="mt-4 border-t-2 border-b-2 boder-gray-200 pt-12 pb-7">
