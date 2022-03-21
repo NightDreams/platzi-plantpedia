6c5292fbb75691a67b502037a38c288cdd0da7f1
NightDreams <yayodcv@gmail.com>
2022-03-21 13:56:39 -0600

    C9 - Protect api routes.  Setup locales, layer authenitifaction , alias, test useSession.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import { appWithTranslation } from 'next-i18next'
 import { useServerStyles } from '@ui/ssr'
 import { UIProvider } from '@ui/Provider'
 import { QueryProvider } from '@api/QueryProvider'
-import { SessionProvider as AuthProvider } from 'next-auth/react'
+import { SessionProvider as AuthProvider } from '@auth/react'
 import '../ui/globals.css'
 
 const NextApp = ({ Component, pageProps }: AppProps) => {
@@ -16,7 +16,7 @@ const NextApp = ({ Component, pageProps }: AppProps) => {
           <Component {...pageProps} />
         </UIProvider>
       </QueryProvider>
-    </AuthProvider >
+    </AuthProvider>
   )
 }
 
--- pages/premium/index.tsx
+++ pages/premium/index.tsx
@@ -1,3 +1,12 @@
+// react components
+import { useState, useEffect } from 'react'
+import { useTranslation } from 'next-i18next'
+
+// Components
+import { AccessDenied } from '@components/AccessDenied'
+// UI
+import { Typography } from '@ui/Typography'
+import { Button } from '@ui/Button'
 import { Layout } from '@components/Layout'
 import { GetServerSideProps } from 'next'
 import { getSession, useSession } from 'next-auth/react'
@@ -9,15 +18,23 @@ export const getServerSideProps: GetServerSideProps<{}> = async (context) => {
       redirect: {
         destination: 'api/auth/signin',
         permanent: false,
-      }
+      },
     }
   }
   return { props: { session } }
 }
 
 function PremiumPage() {
-
   const { data: Session, status } = useSession()
+  const [imageUrl, setImageUrl] = useState<string | null>(null)
+  const [refetchCounter, refetch] = useState(0)
+  const { t } = useTranslation(['page-premium'])
+
+  useEffect(() => {
+    fetch('/api/premium')
+      .then((response) => response.json())
+      .then(({ data }) => setImageUrl(data))
+  }, [refetchCounter])
 
   if (status == 'loading') {
     return null
@@ -25,11 +42,35 @@ function PremiumPage() {
 
   // acceso denegado
   if (Session == null) {
-    return <Layout>acceso denegado</Layout>
+    return <AccessDenied />
   }
 
   // logued
-  return <Layout>Contenido secreto </Layout>
+  return (
+    <Layout title="Premium content">
+      <div className="text-center">
+        <Typography variant="h2">
+          {t('welcome', { name: Session.user?.name })}
+        </Typography>
+        <Typography variant="body2" className="mt-1">
+          {t('hereIsYourPremiumContent')}
+        </Typography>
+        <div className="max-w-lg mx-auto text-center my-8">
+          {imageUrl == null ? null : (
+            <img
+              key="imageUrl"
+              src={imageUrl}
+              alt="Random fox"
+              className="rounded"
+            />
+          )}
+        </div>
+        <Button variant="outlined" onClick={() => refetch((c) => ++c)}>
+          {t('more')}
+        </Button>
+      </div>
+    </Layout>
+  )
 }
 
 export default PremiumPage
